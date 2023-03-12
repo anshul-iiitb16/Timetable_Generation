a6a41e931224a12b3ac9b80f649bbae3f67f01f9
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-03-13 01:28:41 +0530

    Added Code with Basic Initialisations

---
+++ IMT2020039_IMT2020535.ml
@@ -0,0 +1,5 @@
+let course_list = ["C1"; "C2"; "C3"]
+
+let participants_list = ["P1", "P2", "P3", "P4", "P5"]
+
+let par_to_course_map = [("P1", ["C1"; "C3"]), ("P2", ["C1"; "C2"]), ("P3", ["C1"; "C2"]), ("P4", ["C2"]), ("P5", ["C3"])]
