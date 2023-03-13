8606d51c9cf268c754c88802b459feaa9e2ec98c
ShreeyaVenneti <vennetishreeya@gmail.com>
2023-03-13 09:26:37 +0530

    Implemented function to invert the Dictionary

--- IMT2020039_IMT2020535.ml
+++ IMT2020039_IMT2020535.ml
@@ -5,6 +5,7 @@ let participants_list = ["P1", "P2", "P3", "P4", "P5"]
 let par_to_course_map = [("P1", ["C1"; "C3"]), ("P2", ["C1"; "C2"]), ("P3", ["C1"; "C2"]), ("P4", ["C2"]), ("P5", ["C3"])]
 
 
+
 (* Add a key value pair (k, v) to the given dict d. *)
 let rec add_to_dict key value dict = 
   match dict with
@@ -19,4 +20,20 @@ let rec add_to_dict key value dict =
 
 
 
-  
+(* Function to invert the dictionary *)
+let invert_dict dict = 
+  let rec loop dict acc = 
+    match dict with
+      [] -> acc
+    | (k, v) :: t -> 
+                        let rec add_loop k' v' acc1 = 
+                          match v' with
+                            [] -> loop t acc1
+                          | h' :: t' -> let temp = add_to_dict h' k acc1 in 
+                                          add_loop k' t' temp 
+
+
+                        in add_loop k v acc 
+
+  in loop dict []
+    
