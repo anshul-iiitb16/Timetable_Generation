4de6f681b7d0b68253de861604af2860242fa580
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-03-13 09:22:12 +0530

    Implemented add_to_dict function to add key value pair

--- IMT2020039_IMT2020535.ml
+++ IMT2020039_IMT2020535.ml
@@ -3,3 +3,20 @@ let course_list = ["C1"; "C2"; "C3"]
 let participants_list = ["P1", "P2", "P3", "P4", "P5"]
 
 let par_to_course_map = [("P1", ["C1"; "C3"]), ("P2", ["C1"; "C2"]), ("P3", ["C1"; "C2"]), ("P4", ["C2"]), ("P5", ["C3"])]
+
+
+(* Add a key value pair (k, v) to the given dict d. *)
+let rec add_to_dict key value dict = 
+  match dict with
+    [] -> [(key, [value])]
+  | (k, v) :: t -> if key = k then
+                     if List.mem value v = false then
+                       (k, value :: v) :: t
+                     else 
+                       dict
+                   else
+                     (k, v) :: add_to_dict key value t
+
+
+
+  
