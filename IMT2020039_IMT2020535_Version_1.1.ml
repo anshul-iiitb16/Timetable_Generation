723fca17d422b5c9b8df15b758865b822fc08ad0
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-03-13 12:07:27 +0530

    Added function for coloring

--- IMT2020039_IMT2020535_Version_1.1.ml
+++ IMT2020039_IMT2020535_Version_1.1.ml
@@ -77,3 +77,15 @@ let interference_graph dict =
                         in check k v t acc
 
   in loop dict [] 
+
+
+
+(* Naive Algo - Color every course differently*)
+let basic_color list_courses = 
+  let rec loop lc acc =
+    match lc with 
+      [] -> []
+    | h :: t -> (h, acc) :: (loop t (acc+1))
+    
+  in loop list_courses 1
+
