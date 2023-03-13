3d51d25d4ce5f3d9ef0706a5869e82d8142b19de
Anshul Jindal <77500664+anshul-iiitb16@users.noreply.github.com>
2023-03-13 12:05:08 +0530

    Delete coloring.ml

--- coloring.ml
+++
@@ -1,9 +0,0 @@
-(* Naive Algo - Color every course differently*)
-let basic_color list_courses = 
-  let rec loop lc acc =
-    match lc with 
-      [] -> []
-    | h :: t -> (h, acc) :: (loop t (acc+1))
-    
-  in loop list_courses 1
-
